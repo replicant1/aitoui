dd075d0081f0aed6ec0afef0be5ae59937b1d1b0
Rod Bailey <rodney.p.bailey@gmail.com>
2026-07-23 13:38:05 +1000

    Bump app version to 1.2.2 (code 5) (#151)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.aitoui"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
